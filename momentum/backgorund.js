cb52086a9b90748bab49d29ee4c9e2faaa3e5e8e
KCHamp <goddhqj8000@ajou.ac.kr>
2021-09-07 18:54:11 +0900

    오류 img 수정

--- momentum/backgorund.js
+++ momentum/backgorund.js
@@ -1,7 +1,7 @@
 const body = document.querySelector('body')
-IMG_NUM = 21
+IMG_NUM = 18
 function init() {
-  var imgNum = parseInt(Math.random()*100)%IMG_NUM;
+  var imgNum = parseInt(Math.random()*100) % IMG_NUM;
   const img = new Image()
   img.src = `./image/${imgNum}.jpg`
   img.classList.add('bgImage')
